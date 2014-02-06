fe31350bbc22fab8462a5569f9c4bf5244ebefdd
WillFelix <willfelix93@gmail.com>
2014-02-06 19:23:33 -0300

    Ajustes

--- ultimoTreino/index.php
+++ ultimoTreino/index.php
@@ -1,34 +1,21 @@
 <?php
-
 	include("header.php");
 	include("../google_analytics.php");
-	
-	$fb = 0;
-	echo "FB FALSE ->  " . $fb;
-	echo "<script>";
-		echo "alert(document.referrer.match(/facebook[.]com/gi));";
-		$fb = "document.referrer.match(/facebook[.]com/gi)";
-	echo "</script>";
-	
-		echo "FB ->  " . $fb;
+?>
 
-	if ($fb == "facebook.com"){
-		// create the Facebook Graph SDK object
-		require '../src/facebook.php';
-		$facebook = new Facebook(array(
-			'appId'=>'372248136241124', // replace with your value
-			'secret'=>'585350d769d4e1f67a32b5b5c34bd256' // replace with your value
-		));
 
-		$signedRequest = $facebook->getSignedRequest();
-		
-		// Inspect the signed request
-		$unlike = ($signedRequest['page']['liked'] == 1)? "" : "_unlike";
-		include("video" . $unlike . ".php");
+
+<script>
+	if (document.referrer.match(/facebook[.]com/gi) != null){
+		window.location.href = "isFace.php";
 	}else{
-		include("video.php");
+		window.location.href = "noFace.php";
 	}
 
+</script>
+
+
+
+<?php
 	include("../footer.php");
-	
 ?>
