4ca924702e11f8473c4d744ea42954898e02ac57
WillFelix <willfelix93@gmail.com>
2014-02-06 17:27:18 -0300

    Ajustes

--- ultimoTreino/index.php
+++ ultimoTreino/index.php
@@ -4,24 +4,26 @@
 	include("../google_analytics.php");
 	
 	echo "<script>";
-		echo "alert(document.domain);";
-		echo "alert(document.referrer);";
-	echo "</script>";
+		echo "if (document.referrer.match(/facebook[.]com/gi)){";
+			include("video.php");
+		echo "}";
+		echo "else{";
 		
-	// create the Facebook Graph SDK object
-	require '../src/facebook.php';
-	$facebook = new Facebook(array(
-		'appId'=>'372248136241124', // replace with your value
-		'secret'=>'585350d769d4e1f67a32b5b5c34bd256' // replace with your value
-	));
+			// create the Facebook Graph SDK object
+			require '../src/facebook.php';
+			$facebook = new Facebook(array(
+				'appId'=>'372248136241124', // replace with your value
+				'secret'=>'585350d769d4e1f67a32b5b5c34bd256' // replace with your value
+			));
 
-	$signedRequest = $facebook->getSignedRequest();
-	
-	// Inspect the signed request
-	$unlike = ($signedRequest['page']['liked'] == 1)? "" : "_unlike";
-	include("video" . $unlike . ".php");
+			$signedRequest = $facebook->getSignedRequest();
+			
+			// Inspect the signed request
+			$unlike = ($signedRequest['page']['liked'] == 1)? "" : "_unlike";
+			include("video" . $unlike . ".php");
 
-	
+		echo "}";
+	echo "</script>";
 
 	include("../footer.php");
 	
