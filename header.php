c8fb548101614b69d33c8c83dd8683e1440e0637
WillFelix <willfelix93@gmail.com>
2014-02-06 15:13:07 -0300

    Ajustes

---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.facebook.com/2008/fbml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>FacebookCam - PEO</title>
+
+	<link href='http://fonts.googleapis.com/css?family=Lato:400,300italic,300,400italic,700,900' rel='stylesheet' type='text/css' />
+	<link rel="stylesheet" href="style/reset.css" type="text/css"/>
+	<link rel="stylesheet" href="style/screen.css" type="text/css"/>
+</head>
+
+<body>
+	
+	<!-- Facebook Root -->
+	<!-- ========================================================================================================== -->
+	<div id="fb-root"></div>
+	<script>
+		window.fbAsyncInit = function() {
+			FB.init({
+		      appId      : '372248136241124',                  					        // App ID from the app dashboard
+		      channelUrl : 'http://fbcam-peo-v2.herokuapp.com/',	// Channel file for x-domain comms
+		      status     : false,							                                  // Check Facebook Login status
+		      xfbml      : true               							                    // Look for social plugins on the page
+			});
+
+			$(document).trigger('fbload');  //  <---- THIS RIGHT HERE TRIGGERS A CUSTOM EVENT CALLED 'fbload'
+		};
+
+		(function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s); js.id = id;
+			js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1&appId=372248136241124";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+	</script>
+	<!-- ========================================================================================================== -->
---
+++ video.php
@@ -0,0 +1,77 @@
+<section class="general default_template chnl1">
+
+	<div class="block_cntt pin">
+		<div class="channel">Canal 1</div>
+		<div class="fb-like" data-href="https://www.facebook.com/portalexamedeordem?fref=ts" data-colorscheme="light" data-layout="button_count" data-action="like" data-show-faces="false" data-send="false"></div>
+		<img src="img/top02.png">
+	</div>
+
+	<header>
+		<h1 class="logo">
+			<img src="img/logo_fcpeo.png" />
+		</h1>
+		<div class="col_02">
+			<p class="line_01">ASSISTA <span>AO VIVO</span></p>
+			<!-- <h2 class="line_03">TRANSMISSÃO DA GRAVAÇÃO DE AULA</h2> -->
+			<h2 class="line_03">MESA REDONDA - 1&ordf; FASE</h2>
+			<p class="line_04">XII EXAME DE ORDEM UNIFICADO</p>
+		</div>
+	</header>
+
+	<section class="content">
+		<div id='video' class="video">
+			<!-- Video aqui -->
+			<span class="thumb"><img src="img/z_lixo_imgvideo.jpg" /></span>
+			<a href="#" class="btn_play"><img src="img/btn_play.png" /></a>
+		</div>
+	</section>
+
+	<section class="schedule">
+		<img src="img/programacao_cn1.png" />
+ 		<a href="index_chnl2.php" class="channel_2"></a>
+	</section>
+
+	<section class="newsletter_form">
+		<div class="col_01">
+			<p class="line_01">QUER RECEBER INFORMAÇÕES SOBRE <br/>O EXAME DE ORDEM?</p>
+			<p class="line_02">CADASTRE-SE AQUI PARA RECEBER AS ÚLTIMAS DICAS</pbr/> E INFORMAÇÕES PARA A PROVA DA OAB.</p>
+		</div>
+		<div class="col_02">
+			<form action="http://cers.mailee.me/go/add_contact_form" id="mailee-form" method="post">
+				<input name="key" type="hidden" value="994119">
+				<input name="url_ok" type="hidden" value="https://www.facebook.com/portalexamedeordem/app_372248136241124">
+				<input name="url_error" type="hidden" value="https://www.facebook.com/portalexamedeordem/app_372248136241124">
+				<input name="list[]" type="hidden" value="523864">
+
+				<label for="txt_nome">Nome</label>
+				<input name="name" type="text" class="txt" id="txt_nome" />
+				<label for="txt_email">E-mail</label>
+				<input name="email" type="text" class="txt" id="txt_email" />
+				<input class="btn_submit" type="submit" value="Cadastrar" />
+			</form>
+		</div>
+	</section>
+
+	<footer>
+		<a href="http://www.portalexamedeordem.com.br/" target="_blank">www.portalexamedeordem.com.br</a>
+	</footer>
+
+	<div class="fb-comments" data-href="http://www.portalexamedeordem.com.br#mesaredonda_xii_pfase" data-numposts="10" data-width="800"></div>
+</section>
+
+
+<script src="script/jquery-1.9.1.js" type="text/javascript"></script>
+<script src="script/jw6/jwplayer.js" type="text/javascript"></script>
+<script>jwplayer.key="Rf+04i9NkiWHUtD93d4ECBX/KFPYRc1pScFRjYaLymQ=";</script>
+
+<script src="script/videoJW.js" type="text/javascript"></script>
+
+<script>
+
+	var html5 = "http://rsaraiva_ios-i.akamaihd.net/hls/live/203931/cers8/playlist.m3u8";
+	var flash = "http://renatosaraiva-f.akamaihd.net/rsaraiva_1_8@37037";
+	var id = "video";
+
+	jwplayerPlay(id, html5, flash);
+	
+</script>
